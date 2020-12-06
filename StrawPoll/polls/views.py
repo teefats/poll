2862e2a8f3f337bd44874a5159a59e0b625e0e0a
Tunde <tee_fats@yahoo.com>
2020-12-06 14:48:39 +0000

    Added detail page

--- StrawPoll/polls/views.py
+++ StrawPoll/polls/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse, response
-from django.template import loader
+# from django.http import HttpResponse, response
+# from django.template import loader
+from django.shortcuts import render
+from django.http  import Http404
 
 from .models import Question
 # Create your views here.
@@ -15,16 +17,21 @@ from .models import Question
 #     return httpResponse(template.render(context,request))
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html')
     context = {
     'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
     # output = ', '.join([q.question_text for q in latest_question_list])
     
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s"
