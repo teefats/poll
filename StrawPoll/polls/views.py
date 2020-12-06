218ea8dacc583f17e462ebd312f8ac0439d0ff40
Tunde <tee_fats@yahoo.com>
2020-12-06 16:23:08 +0000

    Added results.html

--- StrawPoll/polls/views.py
+++ StrawPoll/polls/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
-from django.http import HttpResponse, response
+from django.http import HttpResponse,HttpResponseRedirect ,response
 # from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http  import Http404
+from django.urls import reverse
 
-from .models import Question
+from .models import  Choice, Question
 # Create your views here.
 
 # def index(request):
@@ -36,10 +37,23 @@ def detail(request, question_id):
 
 def results(request, question_id):
      
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-     # result = get_object_or_404(Choice, pk=question_id)
-    # return render(request, 'polls/results.html', {'result':result})
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    # return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+            'question': question,
+            'error_message': "You didnt select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
