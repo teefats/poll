27417d6319536b64980f90c6cccfccaa99b746ca
Tunde <tee_fats@yahoo.com>
2020-12-06 21:09:17 +0000

    Updated admin

--- StrawPoll/polls/admin.py
+++ StrawPoll/polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None,  {'fields': ['question_text']}), 
+    ('Date information', {'fields':['pub_date']}),]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
 # Register your models here.
--- StrawPoll/polls/views.py
+++ StrawPoll/polls/views.py
@@ -31,7 +31,7 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        return Question.object.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
